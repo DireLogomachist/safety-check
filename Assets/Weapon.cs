445af6f3ebbc0eea98207d761c0468f8a714c0f6
Andy <milleram26@gmail.com>
2019-09-19 23:04:24 -0700

    Weapon rotation first pass

---
+++ Assets/Weapon.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    void Start() {
+        
+    }
+
+    void Update() {
+        
+    }
+
+    public void RotateUp() {
+        transform.RotateAround(transform.position, Camera.main.transform.right, 90);
+    }
+    
+    public void RotateDown() {
+        transform.RotateAround(transform.position, Camera.main.transform.right, -90);
+    }
+    
+    public void RotateLeft() {
+        transform.RotateAround(transform.position, Camera.main.transform.up, 90);
+    }
+    
+    public void RotateRight() {
+        transform.RotateAround(transform.position, Camera.main.transform.up, -90);
+    }
+}
