913c5a1d699cbabc0437def586d7813c47fa39fe
Andy <milleram26@gmail.com>
2019-11-07 01:30:48 -0800

    Rotation lerping with anim curve

--- Assets/Weapon.cs
+++ Assets/Weapon.cs
@@ -5,69 +5,52 @@ using UnityEngine;
 public class Weapon : MonoBehaviour
 {
 
-    public AnimationCurve xCurve = AnimationCurve.EaseInOut(0.0f , 0.0f , 1.0f , 1.0f);
-    public AnimationCurve yCurve = AnimationCurve.EaseInOut(0.0f , 0.0f , 1.0f , 1.0f);
-    public AnimationCurve zCurve = AnimationCurve.EaseInOut(0.0f , 0.0f , 1.0f , 1.0f);
-    
-    Animator ao;
-    Animation anim;
-    AnimationClip clip;
+    public AnimationCurve curve = AnimationCurve.EaseInOut(0.0f , 0.0f , 1.0f , 1.0f);
+    bool lerpFlag = false;
 
     void Start() {
-        ao = GetComponent<Animator>();
-        anim = GetComponent<Animation>();
-        clip = new AnimationClip();
-        clip.legacy = true;
     }
 
     void Update() {
-        
     }
 
     public void RotateUp() {
-        //transform.RotateAround(transform.position, Camera.main.transform.right, 90);
-        Debug.Log("up");
-
-        transform.Rotate(new Vector3(90.0f, 0, 0), Space.World);
-        Vector3 newRot = transform.eulerAngles;
-        transform.Rotate(new Vector3(-90.0f, 0, 0), Space.World);
-
-        //Quaternion.EulerRotation();
-
-        xCurve = AnimationCurve.EaseInOut(0.0f, transform.eulerAngles.x, 1.0f, newRot.x);
-        yCurve = AnimationCurve.EaseInOut(0.0f, transform.eulerAngles.y, 1.0f, newRot.y);
-        zCurve = AnimationCurve.EaseInOut(0.0f, transform.eulerAngles.z, 1.0f, newRot.z);
-
-        clip.ClearCurves();
-        clip.SetCurve("", typeof(Transform), "localRotation.x", xCurve);
-        clip.SetCurve("", typeof(Transform), "localRotation.y", yCurve);
-        clip.SetCurve("", typeof(Transform), "localRotation.z", zCurve);
-
-        anim.AddClip(clip, "up");
-        anim.Play("up");
-
-        // get local rot x,y,z
-        // set curve inits with them
-        // transform from button to get new rots
-        // set end curve values from them
-        // reverse if wrapping around 360 (maybe?)
-        // set clip
-        // let it rip
-
+        Quaternion x = transform.rotation;
+        Quaternion y = Quaternion.Euler(new Vector3(90, 0, 0))*transform.rotation;
+        if(!lerpFlag) StartCoroutine(CurveLerp(x, y, curve));
     }
     
     public void RotateDown() {
-        Debug.Log("down");
-        transform.RotateAround(transform.position, Camera.main.transform.right, -90);
+        Quaternion x = transform.rotation;
+        Quaternion y = Quaternion.Euler(new Vector3(-90, 0, 0))*transform.rotation;
+        if(!lerpFlag) StartCoroutine(CurveLerp(x, y, curve));
     }
     
     public void RotateLeft() {
-        Debug.Log("left");
-        transform.RotateAround(transform.position, Camera.main.transform.up, 90);
+        Quaternion x = transform.rotation;
+        Quaternion y = Quaternion.Euler(new Vector3(0, 90, 0))*transform.rotation;
+        if(!lerpFlag) StartCoroutine(CurveLerp(x, y, curve));
     }
     
     public void RotateRight() {
-        Debug.Log("right");
-        transform.RotateAround(transform.position, Camera.main.transform.up, -90);
+        Quaternion x = transform.rotation;
+        Quaternion y = Quaternion.Euler(new Vector3(0, -90, 0))*transform.rotation;
+        if(!lerpFlag) StartCoroutine(CurveLerp(x, y, curve));
+    }
+
+    public IEnumerator CurveLerp(Quaternion start, Quaternion end, AnimationCurve curve) {
+        float time = 0.8f;
+        float elapsed = 0.0f;
+        lerpFlag = true;
+
+        while(elapsed < time) {
+            elapsed += Time.deltaTime;
+            transform.rotation = Quaternion.Lerp(start, end, curve.Evaluate(elapsed/time));
+            yield return null;
+        }
+
+        transform.rotation = end;
+        lerpFlag = false;
+        yield return null;
     }
 }
