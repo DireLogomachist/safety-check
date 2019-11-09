1f05e04e47d57819c2eebcf99e942da9413dc2fb
Andy <milleram26@gmail.com>
2019-11-09 15:35:58 -0800

    Input buffer, ammo

---
+++ Assets/InputPair.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+public class InputPair
+{
+    public IEnumerator input;
+    public float timestamp;
+
+    public InputPair(IEnumerator i, float t) {
+        input = i;
+        timestamp = t;
+    }
+}
--- Assets/Weapon.cs
+++ Assets/Weapon.cs
@@ -2,46 +2,59 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Weapon : MonoBehaviour
-{
+public class Weapon : MonoBehaviour {
 
     public AnimationCurve curve = AnimationCurve.EaseInOut(0.0f , 0.0f , 1.0f , 1.0f);
-    bool lerpFlag = false;
+    public int ammo = 5;
+
+    Queue<InputPair> inputBuffer = new Queue<InputPair>();
+    float bufferLength = 1.5f;
+    bool inputFlag = false;
 
     void Start() {
     }
 
     void Update() {
+        int c = inputBuffer.Count;
+        for(int i=0; i<c; i++) {
+            if((Time.time - inputBuffer.Peek().timestamp) > bufferLength) {
+                inputBuffer.Dequeue();
+            } else {
+                if(inputFlag == false)
+                    StartCoroutine(inputBuffer.Dequeue().input);
+                break;
+            }
+        }
     }
 
     public void RotateUp() {
-        Quaternion x = transform.rotation;
-        Quaternion y = Quaternion.Euler(new Vector3(90, 0, 0))*transform.rotation;
-        if(!lerpFlag) StartCoroutine(CurveLerp(x, y, curve));
+        QueueInput(WeaponRotate(new Vector3(90, 0, 0)));
     }
-    
+
     public void RotateDown() {
-        Quaternion x = transform.rotation;
-        Quaternion y = Quaternion.Euler(new Vector3(-90, 0, 0))*transform.rotation;
-        if(!lerpFlag) StartCoroutine(CurveLerp(x, y, curve));
+        QueueInput(WeaponRotate(new Vector3(-90, 0, 0)));
     }
-    
+
     public void RotateLeft() {
-        Quaternion x = transform.rotation;
-        Quaternion y = Quaternion.Euler(new Vector3(0, 90, 0))*transform.rotation;
-        if(!lerpFlag) StartCoroutine(CurveLerp(x, y, curve));
+        QueueInput(WeaponRotate(new Vector3(0, 90, 0)));
     }
-    
+
     public void RotateRight() {
+
+        QueueInput(WeaponRotate(new Vector3(0, -90, 0)));
+    }
+
+    public IEnumerator WeaponRotate(Vector3 rot) {
         Quaternion x = transform.rotation;
-        Quaternion y = Quaternion.Euler(new Vector3(0, -90, 0))*transform.rotation;
-        if(!lerpFlag) StartCoroutine(CurveLerp(x, y, curve));
+        Quaternion y = Quaternion.Euler(rot)*transform.rotation;
+        StartCoroutine(CurveLerp(x, y, curve));
+        yield return null;
     }
 
     public IEnumerator CurveLerp(Quaternion start, Quaternion end, AnimationCurve curve) {
         float time = 0.5f;
         float elapsed = 0.0f;
-        lerpFlag = true;
+        inputFlag = true;
 
         while(elapsed < time) {
             elapsed += Time.deltaTime;
@@ -50,7 +63,15 @@ public class Weapon : MonoBehaviour
         }
 
         transform.rotation = end;
-        lerpFlag = false;
+        inputFlag = false;
         yield return null;
+    }
+
+    public void QueueInput(IEnumerator input) {
+        inputBuffer.Enqueue(new InputPair(input, Time.time));
+    }
+
+    public void MagDrop() {
+        if(ammo > 0) ammo = 1;
     }
 }
