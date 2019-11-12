d1c8328be67059c047dd3c0e5897d7c52de0c036
Andy <milleram26@gmail.com>
2019-11-11 23:25:25 -0800

    ActionTrigger script, prefab and texture renaming

---
+++ Assets/ActionTrigger.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class ActionTrigger : MonoBehaviour {
+
+    public UnityEvent function;
+
+    void Start() {
+    }
+
+    void Update() {
+    }
+}
