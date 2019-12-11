2c0ffd9e144f2c94eb86b84228036972a578e8df
Andy <milleram26@gmail.com>
2019-12-11 00:28:59 -0800

    PopupController script, canvas prefab edits

---
+++ Assets/PopupController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PopupController : MonoBehaviour {
+
+    public GameObject SafePopup;
+    public GameObject MagRemovalPopup;
+    public GameObject EjectionPopup;
+    public GameObject MisfirePopup;
+
+    Queue<GameObject> popupBuffer = new Queue<GameObject>();
+
+    void Start() {
+        
+    }
+
+    void Update() {
+        
+    }
+
+    public void Spawn() {
+        GameObject popup = GameObject.Instantiate(SafePopup);
+        popup.transform.parent = transform;
+        popup.transform.GetComponent<RectTransform>().anchoredPosition = new Vector2(0,-70*popupBuffer.Count);
+        popupBuffer.Enqueue(popup);
+    }
+}
