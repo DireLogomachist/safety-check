f4751234e57b042e4da7de5f20bb773701817498
Andy <milleram26@gmail.com>
2020-01-05 01:46:24 -0800

    MK68 triggers

--- Assets/MK68Controller.cs
+++ Assets/MK68Controller.cs
@@ -9,14 +9,48 @@ public class MK68Controller : WeaponController {
     public override void Start() {
         base.Start();
         ActionTrigger[] triggers = GetComponentsInChildren<ActionTrigger>();
-        //triggers[0].function.AddListener();       // Pin
-        //triggers[1].function.AddListener();       // Button
-        //triggers[2].function.AddListener();       // Key
-        //triggers[3].function.AddListener();       // Wire
+        triggers[0].function.AddListener(Pin);      // Pin
+        triggers[1].function.AddListener(Button);   // Button
+        triggers[2].function.AddListener(Key);      // Key
+        triggers[3].function.AddListener(Wire);     // Wire
+
+        StartCoroutine(LaunchSpoon());
     }
 
     public override void Update() {
         base.Update();
+    }
+
+    public void Pin() {
+        QueueInput(PinAction());
+    }
+
+    IEnumerator PinAction() {
+        yield break;
+    }
+
+    public void Button() {
+        QueueInput(ButtonAction());
+    }
+
+    IEnumerator ButtonAction() {
+        yield break;
+    }
+
+    public void Key() {
+        QueueInput(KeyAction());
+    }
+
+    IEnumerator KeyAction() {
+        yield break;
+    }
+
+    public void Wire() {
+        QueueInput(WireAction());
+    }
+
+    IEnumerator WireAction() {
+        yield break;
     }
 
     public void Fire() {
