066d39ffcbb739f4b87689016694c1bafbdb93e4
Andy <milleram26@gmail.com>
2020-01-18 01:03:31 -0800

    Title scene and menu behavior

---
+++ Assets/MenuController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuController : MonoBehaviour {
+
+    public float arrowSpeed = 3.0f;
+
+    int index = 0;
+    GameObject arrowLeft;
+    GameObject arrowRight;
+    float arrowLeftOrigin;
+    float arrowRightOrigin;
+    
+    AudioSource audio;
+    AudioClip menuBlip;
+    AudioClip menuSelect;
+
+    void Start() {
+        arrowLeft = GameObject.Find("UICanvas").transform.Find("Menu").transform.Find("ArrowLeft").gameObject;
+        arrowRight= GameObject.Find("UICanvas").transform.Find("Menu").transform.Find("ArrowRight").gameObject;
+        arrowLeftOrigin = arrowLeft.GetComponent<RectTransform>().anchoredPosition.x;
+        arrowRightOrigin = arrowRight.GetComponent<RectTransform>().anchoredPosition.x;
+
+        audio = GetComponent<AudioSource>();
+        menuBlip = (AudioClip) Resources.Load("audio/ui_menu_blip");
+        menuSelect = (AudioClip) Resources.Load("audio/ui_menu_blip");
+    }
+
+    void Update() {
+        float left = arrowLeftOrigin + 50*Mathf.Sin(Time.time*arrowSpeed);
+        float right = arrowRightOrigin - 50*Mathf.Sin(Time.time*arrowSpeed);
+        arrowLeft.GetComponent<RectTransform>().anchoredPosition = new Vector2(left,index*-600);
+        arrowRight.GetComponent<RectTransform>().anchoredPosition = new Vector2(right,index*-600);
+
+        if(Input.GetKeyDown(KeyCode.DownArrow) || Input.GetKeyDown("s")) {
+            audio.PlayOneShot(menuBlip);
+            index = index + 1;
+            if(index > 2) index = 0;
+        } else if(Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown("w")) {
+            audio.PlayOneShot(menuBlip);
+            index = index - 1;
+            if(index < 0) index = 2;
+        }
+    }
+}
--- Assets/scripts/UIController.cs
+++ Assets/scripts/UIController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class UIController : MonoBehaviour {
@@ -8,15 +9,17 @@ public class UIController : MonoBehaviour {
     public Animator transition;
 
     void Start() {
-        WeaponController weapon = FindObjectsOfType<WeaponController>()[0];
+        if(SceneManager.GetActiveScene().name != "Title") {
+            WeaponController weapon = FindObjectsOfType<WeaponController>()[0];
 
-        transform.GetChild(0).GetComponent<Button>().onClick = new Button.ButtonClickedEvent();
-        transform.GetChild(1).GetComponent<Button>().onClick = new Button.ButtonClickedEvent();
-        transform.GetChild(2).GetComponent<Button>().onClick = new Button.ButtonClickedEvent();
-        transform.GetChild(3).GetComponent<Button>().onClick = new Button.ButtonClickedEvent();
-        transform.GetChild(0).GetComponent<Button>().onClick.AddListener(weapon.RotateUp);
-        transform.GetChild(1).GetComponent<Button>().onClick.AddListener(weapon.RotateDown);
-        transform.GetChild(2).GetComponent<Button>().onClick.AddListener(weapon.RotateLeft);
-        transform.GetChild(3).GetComponent<Button>().onClick.AddListener(weapon.RotateRight);
+            transform.GetChild(0).GetComponent<Button>().onClick = new Button.ButtonClickedEvent();
+            transform.GetChild(1).GetComponent<Button>().onClick = new Button.ButtonClickedEvent();
+            transform.GetChild(2).GetComponent<Button>().onClick = new Button.ButtonClickedEvent();
+            transform.GetChild(3).GetComponent<Button>().onClick = new Button.ButtonClickedEvent();
+            transform.GetChild(0).GetComponent<Button>().onClick.AddListener(weapon.RotateUp);
+            transform.GetChild(1).GetComponent<Button>().onClick.AddListener(weapon.RotateDown);
+            transform.GetChild(2).GetComponent<Button>().onClick.AddListener(weapon.RotateLeft);
+            transform.GetChild(3).GetComponent<Button>().onClick.AddListener(weapon.RotateRight);
+        }
     }
 }
