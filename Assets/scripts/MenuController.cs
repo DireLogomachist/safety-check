331ab7c57e78683a19da7f546fb0d049ccaa15a4
Andy <milleram26@gmail.com>
2020-01-20 14:09:14 -0800

    Menu level and credits edits, menu sfx

--- Assets/scripts/MenuController.cs
+++ Assets/scripts/MenuController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MenuController : MonoBehaviour {
 
@@ -15,35 +16,37 @@ public class MenuController : MonoBehaviour {
     float menuOffset = 67;
     float[] mainOffsets = new float[3];
     float[] levelOffsets = new float[4];
-    float[] creditsOffsets = new float[1];
+    float[] creditsOffsets = new float[3];
     enum MenuLevel {main, level, credits};
     MenuLevel currentMenu = MenuLevel.main;
     
     AudioSource audio;
     AudioClip menuBlip;
-    AudioClip menuSelect;
+    AudioClip menuConfirm;
+    Transform canvas;
 
     void Start() {
-        arrowLeft = GameObject.Find("UICanvas").transform.Find("Arrows").transform.Find("ArrowLeft").gameObject;
-        arrowRight= GameObject.Find("UICanvas").transform.Find("Arrows").transform.Find("ArrowRight").gameObject;
+        canvas = GameObject.Find("UICanvas").transform;
+        arrowLeft = canvas.Find("Arrows").transform.Find("ArrowLeft").gameObject;
+        arrowRight= canvas.Find("Arrows").transform.Find("ArrowRight").gameObject;
         arrowLeftOrigin = arrowLeft.GetComponent<RectTransform>().anchoredPosition.x;
         arrowRightOrigin = arrowRight.GetComponent<RectTransform>().anchoredPosition.x;
         arrowYOrigin = arrowRight.GetComponent<RectTransform>().anchoredPosition.y;
 
         audio = GetComponent<AudioSource>();
         menuBlip = (AudioClip) Resources.Load("audio/ui_menu_blip");
-        menuSelect = (AudioClip) Resources.Load("audio/ui_menu_blip");
+        menuConfirm = (AudioClip) Resources.Load("audio/ui_menu_confirm");
 
         mainOffsets[0] = 60;
         mainOffsets[1] = 0;
         mainOffsets[2] = 50;
-
         levelOffsets[0] = 70;
         levelOffsets[1] = 50;
         levelOffsets[2] = 70;
         levelOffsets[3] = 70;
-
-        creditsOffsets[0] = 500;
+        creditsOffsets[0] = -70;
+        creditsOffsets[1] = -70;
+        creditsOffsets[2] = 70;
     }
 
     void Update() {
@@ -60,18 +63,22 @@ public class MenuController : MonoBehaviour {
             } else if(Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown("w")) {
                 audio.PlayOneShot(menuBlip);
                 index = index - 1;
-                if(index < 0) index = 2;
-            } else if(Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.KeypadEnter)) {
+                if(index < 0) index = mainOffsets.Length-1;
+            } else if(Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.Return)) {
                 if(index == 0) {
                     //Load first level
+                    audio.PlayOneShot(menuConfirm);
+                    StartCoroutine(LevelLoad(1));
                 } else if(index == 1) {
+                    audio.PlayOneShot(menuBlip);
                     currentMenu = MenuLevel.level;
-                    GameObject.Find("UICanvas").transform.Find("MainMenu").gameObject.active = false;
-                    GameObject.Find("UICanvas").transform.Find("LevelMenu").gameObject.active = true;
+                    canvas.Find("MainMenu").gameObject.active = false;
+                    canvas.Find("LevelMenu").gameObject.active = true;
                 } else if(index == 2) {
+                    audio.PlayOneShot(menuBlip);
                     currentMenu = MenuLevel.credits;
-                    GameObject.Find("UICanvas").transform.Find("MainMenu").gameObject.active = false;
-                    GameObject.Find("UICanvas").transform.Find("CreditsMenu").gameObject.active = true;
+                    canvas.Find("MainMenu").gameObject.active = false;
+                    canvas.Find("CreditsMenu").gameObject.active = true;
                 }
                 index = 0;
             }
@@ -88,7 +95,19 @@ public class MenuController : MonoBehaviour {
             } else if(Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown("w")) {
                 audio.PlayOneShot(menuBlip);
                 index = index - 1;
-                if(index < 0) index = 2;
+                if(index < 0) index = levelOffsets.Length-1;
+            } else if(Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.Return)) {
+                if(index == levelOffsets.Length-1) {
+                    audio.PlayOneShot(menuBlip);
+                    currentMenu = MenuLevel.main;
+                    canvas.Find("LevelMenu").gameObject.active = false;
+                    canvas.Find("MainMenu").gameObject.active = true;
+                    index = 0;
+                } else {
+                    //Load selected level
+                    audio.PlayOneShot(menuConfirm);
+                    StartCoroutine(LevelLoad(index+1));
+                }
             }
         } else if(currentMenu == MenuLevel.credits) {
             float left = arrowLeftOrigin + creditsOffsets[index] + 8*Mathf.Sin(Time.time*arrowSpeed);
@@ -103,8 +122,22 @@ public class MenuController : MonoBehaviour {
             } else if(Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown("w")) {
                 audio.PlayOneShot(menuBlip);
                 index = index - 1;
-                if(index < 0) index = 2;
+                if(index < 0) index = creditsOffsets.Length-1;
+            } else if(Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.Return)) {
+                audio.PlayOneShot(menuBlip);
+                if(index == creditsOffsets.Length-1) {
+                    currentMenu = MenuLevel.main;
+                    canvas.Find("CreditsMenu").gameObject.active = false;
+                    canvas.Find("MainMenu").gameObject.active = true;
+                    index = 0;
+                }
             }
         }
+    }
+
+    IEnumerator LevelLoad(int index) {
+        canvas.GetComponent<UIController>().transition.SetTrigger("Transition");
+        yield return new WaitForSeconds(3.0f);
+        SceneManager.LoadScene(index);
     }
 }
